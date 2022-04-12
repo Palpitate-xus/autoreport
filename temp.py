a691e726938693a4f4dc1cf6fc351ef130a80e90
Pola-ris <54174343+Pola-ris@users.noreply.github.com>
2022-04-12 18:46:25 +0800

    add pushplus

--- temp.py
+++ temp.py
@@ -1,8 +1,10 @@
 import time
 from selenium import webdriver
 import os
+import requests
 username = os.environ["USERNAME"]
 password = os.environ["PASSWORD"]
+token = os.environ["TOKEN"]
 options = webdriver.ChromeOptions()
 # 这个是绝对路径
 chrome_options = webdriver.ChromeOptions()
@@ -14,24 +16,35 @@ chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
 driver.get("https://workflow.ecust.edu.cn/default/work/uust/zxxsmryb/mrybtb.jsp")
- 
-# 最大化浏览器
-driver.maximize_window()
-time.sleep(5)
-driver.find_element_by_id("username").send_keys(username)
-time.sleep(5)
-driver.find_element_by_id("password").send_keys(password)
-time.sleep(5)
-driver.find_element_by_class_name("auth_login_btn").click()
-time.sleep(5)
-driver.find_element_by_id("sui-select-swjkzk19").send_keys(1)
-time.sleep(5)
-driver.find_element_by_id("sui-select-xcm4").send_keys(1)
-time.sleep(5)
-driver.find_element_by_id("sui-select-sfycxxwc33").send_keys(1)
-time.sleep(5)
-driver.find_element_by_id("post").click()
-time.sleep(5)
-driver.find_element_by_class_name("layui-layer-btn0").click()
-time.sleep(5)
-driver.close()
+
+try:
+    # 最大化浏览器
+    driver.maximize_window()
+    time.sleep(5)
+    driver.find_element_by_id("username").send_keys(username)
+    time.sleep(5)
+    driver.find_element_by_id("password").send_keys(password)
+    time.sleep(5)
+    driver.find_element_by_class_name("auth_login_btn").click()
+    time.sleep(5)
+    driver.find_element_by_id("sui-select-swjkzk19").send_keys(1)
+    time.sleep(5)
+    driver.find_element_by_id("sui-select-xcm4").send_keys(1)
+    time.sleep(5)
+    driver.find_element_by_id("sui-select-sfycxxwc33").send_keys(1)
+    time.sleep(5)
+    driver.find_element_by_id("post").click()
+    time.sleep(5)
+    driver.find_element_by_class_name("layui-layer-btn0").click()
+    time.sleep(5)
+    driver.close()
+    title = "健康打卡成功"
+    content = username + "已完成健康打卡"
+    url = 'http://pushplus.hxtrip.com/send?token='+token+'&title='+title+'&content='+content
+    requests.get(url)
+except:
+    title = "健康打卡失败"
+    content = username + "未完成健康打卡"
+    url = 'http://pushplus.hxtrip.com/send?token='+token+'&title='+title+'&content='+content
+    requests.get(url)
+
